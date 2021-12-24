bc6b6fe78ba0c932b250fb4662c2e5940f9e893c
Kernc <kerncece@gmail.com>
2021-12-24 13:44:45 +0100

    Install the handler on a couple more elements

--- content.js
+++ content.js
@@ -22,7 +22,7 @@ function install_right_click_handler(el, anchor) {
 
 // Install contextMenu handler on elements with `id` attribute
 for (const el of document.querySelectorAll(`
-         h1[id], h2[id], h3[id], h4[id], h5[id], h6[id], 
+         h1[id], h2[id], h3[id], h4[id], h5[id], h6[id],
          p[id], span[id], blockquote[id], a[id], img[id],
          ul[id], ol[id], dl[id], li[id], dt[id],
          section[id], article[id], aside[id], figure[id], nav[id]`)) {
@@ -35,9 +35,17 @@ for (const el of document.querySelectorAll('a[name] + *')) {
     while (a && !a.name) a = a.previousSibling;  // skip empty/text nodes
     install_right_click_handler(el, a.name);
 }
+for (const el of document.querySelectorAll('a[id] + *')) {
+    var a = el.previousSibling;
+    while (a && !a.name) a = a.previousSibling;  // skip empty/text nodes
+    install_right_click_handler(el, a.id);
+}
 
 // Install contextMenu handler on elements containing an `<a name=...>` tag
 // Install handlers in reverse so the first one actually executes last
 for (const el of Array.from(document.querySelectorAll('a[name]'))) {
     install_right_click_handler(el.parentNode, el.name);
+}
+for (const el of Array.from(document.querySelectorAll('a[id]'))) {
+    install_right_click_handler(el.parentNode, el.id);
 }
