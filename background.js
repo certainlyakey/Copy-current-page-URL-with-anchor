d86813687989e3353daad140bc3c5b7e9f62ae5c
Kernc <kerncece@gmail.com>
2021-12-24 13:44:26 +0100

    Port to WebExtensions API (this time for real)

---
+++ background.js
@@ -0,0 +1,28 @@
+var anchor_url = '';
+
+browser.contextMenus.create({
+    "id": "copy-with-ref",
+    "title": "",
+    "visible": false,
+    "contexts": ["image", "link", "page"],
+    "onclick": (info, tab) => {
+        navigator.clipboard.writeText(anchor_url)
+    }
+});
+
+browser.runtime.onMessage.addListener(async function (message) {
+    if (message.cmd == "hide") {
+        //~ console.log("Hiding menu");
+        browser.contextMenus.update("copy-with-ref", {visible: false});
+        browser.contextMenus.refresh();
+    }
+    else if (message.cmd == "show") {
+        anchor_url = message.url;
+        var fragment = message.anchor.substr(0, 20) + (message.anchor.length > 20 ? '…' : '');
+        browser.contextMenus.update("copy-with-ref", {
+            title: 'Copy anchor link #' + fragment,
+            visible: true,
+        });
+        browser.contextMenus.refresh();
+    }
+});
